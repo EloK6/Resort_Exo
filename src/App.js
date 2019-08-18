9411bbf1b5df66b605344aa79215c3d51911d189
EloK6 <ecassignol@gmail.com>
2019-08-18 12:42:32 +0200

    Routes and NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
-// import './App.css';
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact patch="/" component={Home} />
+        <Route exact patch="/rooms/" component={Rooms} />
+        <Route exact patch="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
