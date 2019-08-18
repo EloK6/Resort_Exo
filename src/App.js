378ce57b7c56b627abc60010c37d1a2125e9fa57
EloK6 <ecassignol@gmail.com>
2019-08-18 16:52:09 +0200

    Banner, Page Error and functionnal component

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
     <>
       <Navbar />
       <Switch>
-        <Route exact patch="/" component={Home} />
-        <Route exact patch="/rooms/" component={Rooms} />
-        <Route exact patch="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
       </Switch>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluwe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
